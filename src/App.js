0426dec3567842cb171be885e70fe83bcabed84f
adityathakureka <aditya.thakur@ekamobility.com>
2025-03-07 09:56:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ekaMobilityIcon from "./Assets/EKA_Buses_Range-1.jpg";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Jenkins Test 1</h1>
+      <h1 className="text-center">Jenkins Test 2</h1>
       <div className="container">
         <p className="text-center">
           This is a React File made with the help of Bootstrap.
